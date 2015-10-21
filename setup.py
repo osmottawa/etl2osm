1fb2b30479eb7c54d37a9b751d4c786a5a1bc0c2
Denis <carriere.denis@gmail.com>
2015-10-21 19:16:03 -0400

    Place the fiona before install

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if not version:
 with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
 
-requires = ['click', 'six', 'fiona']
+requires = ['click', 'six']
 
 setup(
     name='etl2osm',
