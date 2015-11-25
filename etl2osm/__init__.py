84ec84494537e109b927ec3b14922130ec314525
Denis <carriere.denis@gmail.com>
2015-11-25 14:44:50 -0500

    0.0.2 Version change

--- etl2osm/__init__.py
+++ etl2osm/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 __title__ = 'etl2osm'
 __author__ = 'OSM Canada Team'
 __author_email__ = 'carriere.denis@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2015 OSM Canada Team'
 
